5b9ec3eee495d4aca0ae9ee0f04b8b3255ac0353
HackByMinh <98638554+minhbl@users.noreply.github.com>
2022-02-12 13:24:52 +0700

    Update minhblusV1.lua

--- minhblusV1.lua
+++ minhblusV1.lua
@@ -1,71 +1,133 @@
-local Minhblus = {}
+local minhblus = {}
+
+function minhblus:CreLib(name,verr)
 local UI = Instance.new("ScreenGui")
 local Main = Instance.new("Frame")
+local UICorner = Instance.new("UICorner")
+local Tabs = Instance.new("Frame")
+local UICorner_2 = Instance.new("UICorner")
+local TabMain = Instance.new("ScrollingFrame")
+local UIListLayout = Instance.new("UIListLayout")
+local Tabs_2 = Instance.new("TextButton")
+local UICorner_3 = Instance.new("UICorner")
+local MainSecl = Instance.new("Frame")
+local UICorner_4 = Instance.new("UICorner")
+local Secl = Instance.new("ScrollingFrame")
+local UIListLayout_2 = Instance.new("UIListLayout")
+local NameUi = Instance.new("TextLabel")
+local Ver = Instance.new("TextLabel")
 
-function Minhblus:CreateLib(Name)
 UI.Name = "UI"
-UI.Parent = game:GetService("CoreGui")
+UI.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
 UI.ZIndexBehavior = Enum.ZIndexBehavior.Sibling
 
 Main.Name = "Main"
 Main.Parent = UI
-Main.Active = true
-Main.Draggable = true
-Main.BackgroundColor3 = Color3.fromRGB(0, 0, 0)
-Main.Position = UDim2.new(0.287729204, 0, 0.204933584, 0)
-Main.Size = UDim2.new(0, 300, 0, 311)
+Main.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+Main.BorderSizePixel = 0
+Main.Position = UDim2.new(0.192034125, 0, 0.216318801, 0)
+Main.Size = UDim2.new(0, 432, 0, 298)
 
 UICorner.CornerRadius = UDim.new(0, 4)
 UICorner.Parent = Main
 
-ManSec.Name = "ManSec"
-ManSec.Parent = Main
-ManSec.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-ManSec.BorderSizePixel = 0
-ManSec.Position = UDim2.new(0.0332519524, 0, 0.148470581, 0)
-ManSec.Size = UDim2.new(0, 280, 0, 252)
+Tabs.Name = "Tabs"
+Tabs.Parent = Main
+Tabs.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+Tabs.BorderSizePixel = 0
+Tabs.Position = UDim2.new(0.020833334, 0, 0.110738255, 0)
+Tabs.Size = UDim2.new(0, 100, 0, 254)
 
-UIListLayout.Parent = ManSec
+UICorner_2.CornerRadius = UDim.new(0, 4)
+UICorner_2.Parent = Tabs
+
+TabMain.Name = "TabMain"
+TabMain.Parent = Tabs
+TabMain.Active = true
+TabMain.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+TabMain.BackgroundTransparency = 1.000
+TabMain.BorderSizePixel = 0
+TabMain.Position = UDim2.new(0, 0, 0.0433070883, 0)
+TabMain.Size = UDim2.new(0, 100, 0, 230)
+TabMain.BottomImage = ""
+TabMain.MidImage = ""
+TabMain.TopImage = ""
+
+UIListLayout.Parent = TabMain
+UIListLayout.HorizontalAlignment = Enum.HorizontalAlignment.Center
 UIListLayout.SortOrder = Enum.SortOrder.LayoutOrder
-UIListLayout.Padding = UDim.new(0, 3)
+UIListLayout.Padding = UDim.new(0, 8)
 
-local toggle = {}
-function toggle:ToggleFrame(namee)
-    
-ToggerFrame.Name = "ToggerFrame"
-ToggerFrame.Parent = ManSec
-ToggerFrame.BackgroundColor3 = Color3.fromRGB(20, 20, 20)
-ToggerFrame.BackgroundTransparency = 1.000
-ToggerFrame.BorderSizePixel = 0
-ToggerFrame.Size = UDim2.new(0, 119, 0, 37)
+Tabs_2.Name = "Tabs"
+Tabs_2.Parent = TabMain
+Tabs_2.BackgroundColor3 = Color3.fromRGB(35, 35, 35)
+Tabs_2.BorderSizePixel = 0
+Tabs_2.Position = UDim2.new(0.159999996, 0, 0, 0)
+Tabs_2.Size = UDim2.new(0, 77, 0, 35)
+Tabs_2.Font = Enum.Font.Cartoon
+Tabs_2.Text = "Main"
+Tabs_2.TextColor3 = Color3.fromRGB(255, 255, 255)
+Tabs_2.TextScaled = true
+Tabs_2.TextSize = 14.000
+Tabs_2.TextWrapped = true
 
-Togger.Name = "Toggle"
-Togger.Parent = ToggerFrame
-Togger.BackgroundColor3 = Color3.fromRGB(255, 0, 0)
-Togger.BorderColor3 = Color3.fromRGB(50, 50, 50)
-Togger.BorderSizePixel = 2
-Togger.Position = UDim2.new(0.649999976, 0, 0, 0)
-Togger.Size = UDim2.new(0, 45, 0, 37)
-Togger.Font = Enum.Font.Cartoon
-Togger.Text = ""
-Togger.TextColor3 = Color3.fromRGB(255, 255, 255)
-Togger.TextScaled = true
-Togger.TextSize = 14.000
-Togger.TextWrapped = true
+UICorner_3.CornerRadius = UDim.new(0, 4)
+UICorner_3.Parent = Tabs_2
 
-Name.Name = "Name"
-Name.Parent = ToggerFrame
-Name.BackgroundColor3 = Color3.fromRGB(15, 15, 15)
-Name.BorderSizePixel = 0
-Name.Position = UDim2.new(-0.00533726811, 0, 0, 0)
-Name.Size = UDim2.new(0, 74, 0, 37)
-Name.Font = Enum.Font.Cartoon
-Name.Text = namee
-Name.TextColor3 = Color3.fromRGB(255, 255, 255)
-Name.TextScaled = true
-Name.TextSize = 14.000
-Name.TextWrapped = true
+MainSecl.Name = "MainSecl"
+MainSecl.Parent = Main
+MainSecl.BackgroundColor3 = Color3.fromRGB(60, 60, 60)
+MainSecl.BorderSizePixel = 0
+MainSecl.Position = UDim2.new(0.280092597, 0, 0.110738255, 0)
+MainSecl.Size = UDim2.new(0, 298, 0, 254)
+
+UICorner_4.CornerRadius = UDim.new(0, 4)
+UICorner_4.Parent = MainSecl
+
+Secl.Name = "Secl"
+Secl.Parent = MainSecl
+Secl.Active = true
+Secl.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Secl.BackgroundTransparency = 1.000
+Secl.BorderSizePixel = 0
+Secl.Position = UDim2.new(0.0402684547, 0, 0.0433070883, 0)
+Secl.Size = UDim2.new(0, 286, 0, 229)
+Secl.BottomImage = ""
+Secl.MidImage = ""
+Secl.TopImage = ""
+
+UIListLayout_2.Parent = Secl
+UIListLayout_2.SortOrder = Enum.SortOrder.LayoutOrder
+UIListLayout_2.Padding = UDim.new(0, 8)
+
+
+NameUi.Name = "NameUi"
+NameUi.Parent = Main
+NameUi.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+NameUi.BackgroundTransparency = 1.000
+NameUi.BorderSizePixel = 0
+NameUi.Position = UDim2.new(0.020833334, 0, 0, 0)
+NameUi.Size = UDim2.new(0, 100, 0, 33)
+NameUi.Font = Enum.Font.Cartoon
+NameUi.Text = name
+NameUi.TextColor3 = Color3.fromRGB(255, 255, 255)
+NameUi.TextScaled = true
+NameUi.TextSize = 14.000
+NameUi.TextWrapped = true
+NameUi.TextXAlignment = Enum.TextXAlignment.Left
+
+Ver.Name = "Ver"
+Ver.Parent = Main
+Ver.BackgroundColor3 = Color3.fromRGB(255, 255, 255)
+Ver.BackgroundTransparency = 1.000
+Ver.BorderSizePixel = 0
+Ver.Position = UDim2.new(0.73842591, 0, 0, 0)
+Ver.Size = UDim2.new(0, 100, 0, 33)
+Ver.Font = Enum.Font.Cartoon
+Ver.Text = verr
+Ver.TextColor3 = Color3.fromRGB(255, 255, 255)
+Ver.TextScaled = true
+Ver.TextSize = 14.000
+Ver.TextWrapped = true
 end
-return toggle
-end
-return Minhblus
+return minhblus
