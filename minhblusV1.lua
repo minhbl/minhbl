19c79a6ceab562c5de27a5a25de4259380136f38
HackByMinh <98638554+minhbl@users.noreply.github.com>
2022-02-20 21:27:18 +0700

    Update minhblusV1.lua

--- minhblusV1.lua
+++ minhblusV1.lua
@@ -1,4 +1,5 @@
 local Hub = {}
+local Tabs = {}
 
 function Hub:CreateLib(namelib)
 namelib = namelib or "Name"
@@ -40,7 +41,7 @@ NameUI.BorderSizePixel = 0
 NameUI.Position = UDim2.new(0.023584906, 0, 0.0199335553, 0)
 NameUI.Size = UDim2.new(0, 95, 0, 29)
 NameUI.Font = Enum.Font.SourceSans
-NameUI.Text = namelib
+NameUI.Text = name
 NameUI.TextColor3 = Color3.fromRGB(205, 205, 205)
 NameUI.TextScaled = true
 NameUI.TextSize = 14.000
@@ -91,7 +92,7 @@ Thanh_5.Size = UDim2.new(0, 316, 0, 2)
 
 Page.Name = "Page"
 Page.Parent = Main
-function Hub:CreateTab(name)
+function Tabs:CreateTab(name)
 local UIListLayout_2 = Instance.new("UIListLayout")
 local Page_2 = Instance.new("Frame")
 local TextButton = Instance.new("TextButton")
@@ -128,5 +129,6 @@ TextButton.MouseButton1Click:Connect(function()
     Page_2.Visible = true
 end)
 end
+return Tabs
 end
 return Hub
