9fb7ff80d41f6640e9919610db095c5c3f505c31
HackByMinh <98638554+minhbl@users.noreply.github.com>
2022-02-21 16:00:32 +0700

    Update minhblusV1.lua

--- minhblusV1.lua
+++ minhblusV1.lua
@@ -25,10 +25,10 @@ function CircleClick(Button, X, Y)
 		elseif Button.AbsoluteSize.X == Button.AbsoluteSize.Y then																																																																														print("This place uses a model by Come0n.") --please do not remove!
 			Size = Button.AbsoluteSize.X * 1.5
 		end
-		local Time = 0.2
-		Circle:TweenSizeAndPosition(UDim2.new(0, Size, 0, Size), UDim2.new(0.5, - Size / 2, 0.5, -Size/2), "InOut", "Quint", Time, false, nil)
+		local Time = 0.5
+		Circle:TweenSizeAndPosition(UDim2.new(0, Size, 0, Size), UDim2.new(0.5, - Size / 2, 0.5, -Size/2), "Out", "Quad", Time, false, nil)
 		for Index = 1, 10 do
-			Circle.ImageTransparency = Circle.ImageTransparency + 0.01
+			Circle.ImageTransparency = Circle.ImageTransparency + 0.1
 			wait(Time / 10)
 		end
 		Circle:Destroy()
