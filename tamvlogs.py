ed53f1004725b36dbcbf93d61ef76afbd5a13833
HackByMinh <98638554+minhbl@users.noreply.github.com>
2024-07-07 15:57:36 +0700

    Update tamvlogs.py

--- tamvlogs.py
+++ tamvlogs.py
@@ -2,6 +2,7 @@ import requests
 import json
 import time
 import sys
+import threading
 
 print('') 
 ids=[]
@@ -15,18 +16,36 @@ print("=====tam dz co dep trai khong=====")
 print("=====co dep trai la co dep trai=====")
 
 
-getkeyurl="http://45.90.13.151:6041/?url="
-deltaurl="https://gateway.platoboost.com/a/8?id="
+
+payload = {
+    "captcha":"",
+    "type":"Turnstile"
+}
+code = "79ca"
+
+def getkey(id):
+    keydata=""
+    session = requests.Session()
+    session.post(f"https://api-gateway.platoboost.com/v1/sessions/auth/8/{id}", json=payload)
+    session.put(f"https://api-gateway.platoboost.com/v1/sessions/auth/8/{id}/{code}")
+    time.sleep(5)
+    session.put(f"https://api-gateway.platoboost.com/v1/sessions/auth/8/{id}/{code}")
+    response = session.get(f"https://api-gateway.platoboost.com/v1/authenticators/8/{id}").text
+    keydata=json.loads(response)["key"]
+
+
+    print(f"[tam dz] id {id} key {keydata}")
 
 while True:
+    allthread=[]
     for id in ids:
-        print(f"[tam dz] get key {str(id)}")
-        keykey=requests.get(getkeyurl+deltaurl+str(id))
-        json_data = json.loads(keykey.content.decode('utf-8'))
-        print(f"[tam dz] key {json_data["key"]}")
-        print(f"[tam dz] done {str(id)}")
-        time.sleep(0.5)
+        thread1=threading.Thread(target=getkey,args=(str(id),))
+        allthread.append(thread1)
+        thread1.start()
+    for threadd in allthread:
+        threadd.join()
     for i in range(1800, -1, -1):
         sys.stdout.write(f"\r===== Chờ tam dz {i} giây để tiếp tục tool =====")
         sys.stdout.flush()
         time.sleep(1)
+    print("")
