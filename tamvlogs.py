fc3183ce86b8ef2e94d703dab65c9877df93a80c
HackByMinh <98638554+minhbl@users.noreply.github.com>
2024-07-07 17:00:10 +0700

    Update tamvlogs.py

--- tamvlogs.py
+++ tamvlogs.py
@@ -2,7 +2,15 @@ import requests
 import json
 import time
 import sys
-import threading
+import requests
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+def fetch(url):
+    try:
+        response = requests.get(url)
+        return response.status_code, url
+    except requests.RequestException as e:
+        return None, url
 
 print('') 
 ids=[]
@@ -17,33 +25,33 @@ print("=====co dep trai la co dep trai=====")
 
 
 
-payload = {
-    "captcha":"",
-    "type":"Turnstile"
-}
-code = "79ca"
+getkeyurl="http://45.90.13.151:6041/?url="
+deltaurl="https://gateway.platoboost.com/a/8?id="
 
-def getkey(id):
-    keydata=""
-    session = requests.Session()
-    session.post(f"https://api-gateway.platoboost.com/v1/sessions/auth/8/{id}", json=payload)
-    session.put(f"https://api-gateway.platoboost.com/v1/sessions/auth/8/{id}/{code}")
-    time.sleep(5)
-    session.put(f"https://api-gateway.platoboost.com/v1/sessions/auth/8/{id}/{code}")
-    response = session.get(f"https://api-gateway.platoboost.com/v1/authenticators/8/{id}").text
-    keydata=json.loads(response)["key"]
-
-
-    print(f"[tam dz] id {id} key {keydata}")
+def fetch_key(id):
+    id = str(id)
+    try:
+        keykey = requests.get(getkeyurl + deltaurl + id)
+        json_data = json.loads(keykey.content.decode('utf-8'))
+        return id, json_data.get("key", None)
+    except requests.RequestException as e:
+        return id, None
+max_workers = 10
 
 while True:
-    allthread=[]
-    for id in ids:
-        thread1=threading.Thread(target=getkey,args=(str(id),))
-        allthread.append(thread1)
-        thread1.start()
-    for threadd in allthread:
-        threadd.join()
+    with ThreadPoolExecutor(max_workers=max_workers) as executor:
+        future_to_id = {executor.submit(fetch_key, id): id for id in ids}
+        
+        for future in as_completed(future_to_id):
+            id = future_to_id[future]
+            try:
+                id, key = future.result()
+                if key:
+                    print(f"[tam dz] id {id} key {key}")
+                else:
+                    print(f"[tam dz] id {id} failed to get key")
+            except Exception as e:
+                print(f"[tam dz] id {id} generated an exception: {e}")
     for i in range(1800, -1, -1):
         sys.stdout.write(f"\r===== Chờ tam dz {i} giây để tiếp tục tool =====")
         sys.stdout.flush()
